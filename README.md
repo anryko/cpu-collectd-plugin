af763a3e998c4ece5b9cf9309a9c859ca8e2b4d0
anryko <a.shvenke@gmail.com>
2015-10-16 16:27:43 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # cpu-collectd-plugin
+
+Collectd linux CPU states tracking plugin.
+
+#### Motivation
+Default CollectD CPU plugin graphing is complicated. Plugin stores information per thread which is not relevant to my use case and in case when I have 32 threads on the machine it takes quite a while to query 8x32 metrics. I want to have aggregated metrics in 'percent' ready to graph.
+
+#### Metrics
+```
+user
+nice
+system
+idle
+wait
+interrupt
+softirq
+steal
+guest
+guestn
+```
---
+++ cpu_info.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+
+import collectd
+import time
+
+
+# Global variables
+
+NAME = 'cpu_info'
+VERBOSE = True
+
+
+# Helper functions
+
+def get_stats():
+    """ Reads /proc/stat and returns array of cpu stats.
+    """
+    with open('/proc/stat', 'r') as f:
+        arr = f.readline().split()
+    arr.pop(0)
+    return ([float(a) for a in arr ] + [0] * 10)[:10]
+
+
+def log(t, message):
+    """ Log messages to collectd logger
+    """
+    if t == 'err':
+        collectd.error('{}: {}'.format(NAME, message))
+    elif t == 'warn':
+        collectd.warning('{}: {}'.format(NAME, message))
+    elif t == 'verb':
+        if VERBOSE:
+            collectd.info('{}: {}'.format(NAME, message))
+    else:
+        collectd.info('{}: {}'.format(NAME, message))
+
+
+def configure_callback(conf):
+    """ Config data from collectd
+    """
+    log('verb', 'configure_callback Running')
+    global NAME, VERBOSE
+    for node in conf.children:
+        if node.key == 'Name':
+            NAME = node.values[0]
+        elif node.key == 'Verbose':
+            VERBOSE = node.values[0]
+        else:
+            log('warn', 'Unknown config key: {}'.format(node.key))
+
+
+def read_callback():
+    """ Prepare data for collectd
+    """
+    log('verb', 'read_callback Running')
+
+    stats = get_cpustats()
+
+    if not stats:
+        log('verb', 'No statistics received')
+        return
+
+    for metric, percent in stats:
+        log('verb', 'Sending value: {} {}'.format(metric, percent))
+        value = collectd.Values(plugin=NAME)
+        value.type = 'percent'
+        value.type_instance = metric
+        value.values = [ str(percent) ]
+        value.dispatch()
+
+
+def get_cpustats():
+    """ Get CPU stats in percentages
+    """
+    cpu = get_stats()
+    time.sleep(5)
+    cpun = get_stats()
+
+    deltas = [ n[0] - n[1] for n in zip(cpun, cpu) ]
+    dj = sum(deltas)
+    percent = [ ((d / dj) * 100) for d in deltas ]
+
+    columns = [
+        'user',
+        'nice',
+        'system',
+        'idle',
+        'wait',
+        'interrupt',
+        'softirq',
+        'steal',
+        'guest',
+        'guestn'
+    ]
+
+    return zip(columns, percent)
+
+
+# Register to collectd
+collectd.register_config(configure_callback)
+collectd.warning('Initialising {}'.format(NAME))
+collectd.register_read(read_callback)
